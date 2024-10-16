530da754018a3d27da68eb00821fc354e232377e
Md. Yeasin Sheikh <yeasinsheikh50@gmail.com>
2024-10-16 12:37:36 +0600

    fixed-splash-color

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'dart:ui' as ui;
 
 import 'package:sun_moon_theme_switch_button/load_assest_image.dart';
 
+import 'moon_sun_switch_painter.dart';
+
 void main() {
   runApp(const MaterialApp(home: MainApp()));
 }
@@ -56,154 +58,40 @@ class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
       body: Center(
         child: moon == null
             ? const CircularProgressIndicator()
-            : InkWell(
-                onTap: onTap,
-                child: AspectRatio(
-                  aspectRatio: 3 / 1,
-                  child: CustomPaint(
-                    painter: MoonPainter(
-                      animation: controller,
-                      moon: moon!,
-                      cloudT: cloud1,
-                      cloudB: cloud2,
-                      star: star,
-                    ),
+            : AspectRatio(
+                aspectRatio: 3 / 1,
+                child: Material(
+                  shape: const StadiumBorder(),
+                  clipBehavior: Clip.antiAlias,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      CustomPaint(
+                        painter: MoonPainter(
+                          animation: controller,
+                          moon: moon!,
+                          cloudT: cloud1,
+                          cloudB: cloud2,
+                          star: star,
+                        ),
+                      ),
+                      Material(
+                        color: Colors.transparent,
+                        shape: const StadiumBorder(),
+                        clipBehavior: Clip.antiAlias,
+                        child: InkWell(
+                          customBorder: const StadiumBorder(),
+                          splashColor: Colors.white.withOpacity(0.3),
+                          highlightColor: Colors.transparent,
+                          onTap: onTap,
+                          child: const SizedBox(),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
       ),
     );
   }
-}
-
-double degToRad(double deg) => deg * 3.1416 / 180;
-
-class MoonPainter extends CustomPainter {
-  const MoonPainter({
-    required this.animation,
-    required this.moon,
-    required this.cloudT,
-    required this.cloudB,
-    required this.star,
-  }) : super(repaint: animation);
-
-  final Animation animation;
-  final ui.Image moon;
-  final ui.Image cloudT, cloudB;
-  final ui.Image star;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final double progress = animation.value;
-    final bgColor = Color.lerp(const Color(0xFF0F405B), const Color(0xFF5BA9D3), progress)!;
-    canvas.drawRect(
-      Rect.fromLTWH(0, 0, size.width, size.height),
-      Paint()..color = bgColor,
-    );
-
-    final moonSize = Size.square(size.height - 10);
-
-    final moonPosition = Offset(
-      (size.width - moonSize.width) * progress + moonSize.width / 2,
-      size.height / 2,
-    );
-
-    final Path moonPath = Path()
-      ..addOval(Rect.fromCenter(
-        center: const Offset(0, 0),
-        width: moonSize.width,
-        height: moonSize.height,
-      ));
-
-    final Path cutOutPath = Path()
-      ..addOval(Rect.fromCenter(
-        center: Offset(ui.lerpDouble(moonSize.width / 3, moonSize.width * 1.5, progress)!, -20),
-        width: moonSize.width,
-        height: moonSize.height,
-      ));
-    final Path combinedPath = Path.combine(PathOperation.difference, moonPath, cutOutPath);
-
-    canvas.save();
-
-    canvas.translate(moonPosition.dx, moonPosition.dy);
-    canvas.rotate(degToRad(360 * progress));
-
-    canvas.clipPath(combinedPath);
-
-    Paint paint = Paint();
-
-    if (progress > .5) {
-      final alpha = ui.lerpDouble(0, 255, progress)!.toInt();
-      paint.colorFilter = ColorFilter.mode(
-        const Color(0xFFF6D35A).withAlpha(alpha),
-        BlendMode.srcATop,
-      );
-    }
-    canvas.drawImageRect(
-      moon,
-      Rect.fromLTWH(0, 0, moon.width.toDouble(), moon.height.toDouble()),
-      Rect.fromCenter(center: const Offset(0, 0), width: moonSize.width, height: moonSize.height),
-      paint,
-    );
-    canvas.restore();
-
-    //*Stars
-    double starWidth = star.width.toDouble();
-    double starHeight = star.height.toDouble();
-
-    double initialStarRatio = size.height / starHeight;
-    double scaleFactor = ui.lerpDouble(initialStarRatio, 0, progress)!;
-    canvas.drawImageRect(
-      star,
-      Rect.fromLTWH(0, 0, starWidth, starHeight),
-      Rect.fromLTWH(
-        size.width - starWidth * scaleFactor,
-        (size.height - starHeight * scaleFactor) / 2,
-        starWidth * scaleFactor,
-        starHeight * scaleFactor,
-      ),
-      Paint(),
-    );
-
-    //* Clouds1
-
-    double cBw = cloudB.width.toDouble();
-    double cBh = cloudB.height.toDouble();
-
-    final cloudPaint = Paint()
-      ..color = Colors.white.withAlpha(ui.lerpDouble(0, 255, progress)!.toInt())
-      ..blendMode = ui.BlendMode.srcOver;
-
-    double cloudScale = (size.height / cBh) * .8;
-
-    canvas.drawImageRect(
-      cloudB,
-      Rect.fromLTWH(0, 0, cBw, cBh),
-      Rect.fromLTWH(
-        (cBw * cloudScale * .35),
-        ui.lerpDouble(size.height, size.height - (cBh * cloudScale) * .75, progress)!,
-        cBw * cloudScale,
-        cBh * cloudScale,
-      ),
-      cloudPaint,
-    );
-
-    double cTw = cloudT.width.toDouble();
-    double cTh = cloudT.height.toDouble();
-
-    canvas.drawImageRect(
-      cloudT,
-      Rect.fromLTWH(0, 0, cTw, cTh),
-      Rect.fromLTWH(
-        ui.lerpDouble(-cTw * cloudScale, -cTw * cloudScale * .10, progress)!,
-        size.height - (cTh * cloudScale) + cTw * cloudScale * .10,
-        cTw * cloudScale,
-        cTh * cloudScale,
-      ),
-      cloudPaint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
